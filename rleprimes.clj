a40e83e4d92d86f08b7aa259699372774cd6e9d0
Peter Benson <ben21033@byui.edu>
2022-06-14 18:23:22 +0000

    week 5 commit 1

--- rleprimes.clj
+++ rleprimes.clj
@@ -13,13 +13,11 @@
         ))
 )
 (defn -main[]
-  (def primes '(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97))
   (def rleprimes '(1 1 0 1 0 1 3 1 0 1 3 1 0 1 3 1 5 1 0 1 5 1 3 1 0 1 3 1 5 1 5 1 0 1 5 1 3 1 0 1 5 1 3 1 5 1 7 1 3))
 
   ;; Decodes the rleprimes.
   (println "Encrypted: "rleprimes)
   (println "Unencrypted:")
   (println (decode-prime-list rleprimes))
-  ;; sees if primes and rleprimes are the same or not. If not then it will be false. If so then it will be true.
-  ;;(println (= primes (decode-prime-list rleprimes)))
+
 )
